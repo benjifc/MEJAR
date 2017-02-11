cb195f5403ae6205af9f6e1b792979a91fb074ac
bfernandez <benjifc@gmail.com>
2017-02-11 14:48:16 +0100

    Bash Script Mongo Server

--- mongo-server.sh
+++ mongo-server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mongod  --fork --logpath mongo.log --smallfiles --oplogSize 50 --port 27001 --dbpath data/mongo 
+mongod  --fork --logpath data/mongo/log/mongo.log --smallfiles --oplogSize 50 --port 27001 --dbpath data/mongo 
